4cf68476e3c1c821a6404dc43f4b71fdd1ad2d96
Green <greenbcs@gmail.com>
2018-04-26 23:58:11 +0800

    3rd modification

--- guyu/guyu/3000setpoints/Main_New.cpp
+++ guyu/guyu/3000setpoints/Main_New.cpp
@@ -19,7 +19,7 @@
 #include <iostream>
 #include <fstream>
 #include "globle_pars.h"
-#include "mysql++.h"
+#include <mysql++.h>
 #include <string>
 #include <sstream>
 using namespace std;
@@ -28,9 +28,9 @@ using namespace mysqlpp;
 #define DATEBASE_IP "119.23.229.99"
 #define DATEBASE_USERNAME "pmttest"
 #define DATEBASE_PWD "juno20170516"
+#define DATABASE_TABLE "container_test"
 
-
-int  Main_New(char *inDirectory)
+int Main_New(char *inDirectory)
 {
 //********open the folder and print it in the a txt file********
 //********call the open_directory_new function*****************
@@ -122,53 +122,54 @@ outFile.open(tmpFileName);
 outFile<<"Channel"<<"  "<<"Mass_ID"<<"  "<<"PMT_ID"<<"  "<<"MCP_Hama"<<"  "<<"HV"<<"  "<<"Gain"<<"  "<<"Mu"<<"  "<<"CF"<<"  "<<"Mu_charge"<<"  "<<"CF_charge"<<"  "<<"Mean_charge"<<"  "<<"Amplitude"<<"  "<<"Resolution"<<"  "<<"P/V"<<"  "<<"S/N"<<"  "<<"Risetime"<<"  "<<"Falltime"<<"  "<<"FWHM"<<"  "<<"Prepulse_proportion"<<"  "<<"Dark_rate"<<"  "<<"Pedestal_chi2_ndf"<<"  "<<"Signal_chi2_ndf"<<"  "<<"PDE"<<"  "<<"Hittime"<<"  "<<"TTS"<<"  "<<"Prepulse_proportion_sub"<<endl;
 for(int i=101;i<137;i++)
     {
+	Connection conn(false);
     if (i!=131)
        {
         outFile<<i<<"  "<<massNumber<<"  "<<PMT_ID[i]<<"  "<<Mcp_Hama[i]<<"  "<<HV[i]<<"  "<<Gain[i]<<"  "<<Mu[i]<<"  "<<CF[i]<<"  "<<Mu_charge[i]<<"  "<<CF_charge[i]<<"  "<<Mean_charge[i]<<"  "<<Amplitude[i]<<"  "<<Resolution[i]<<"  "<<P_V[i]<<"  "<<S_N[i]<<"  "<<Risetime[i]<<"  "<<Falltime[i]<<"  "<<FWHM_NEW[i]<<"  "<<Prepulse_proportion[i]<<"  "<<Dark_rate[i]<<"  "<<Pedestal_chi2_ndf[i]<<"  "<<Signal_chi2_ndf[i]<<"  "<<PDE[i]<<"  "<<Hittime[i]<<"  "<<TTS[i]<<"  "<<Prepulse_proportion_sub[i]<<endl;
-	Connection conn(false);
     	if (conn.connect(DATEBASE_NAME, DATEBASE_IP, DATEBASE_USERNAME, DATEBASE_PWD))
-	{
+		{
 		time_t now = time(0);
-	    	/* ??now è?为å¬?¸²形å*/
 		char Day[45];
 		strftime(Day, sizeof(Date), "%Y-%m-%d", localtime(&now));
 		string Date;
 		Date = Day;
-		//cout << Date << endl;
 		stringstream SS;
 		SS.str("");
-		SS << "SELECT * FROM container_test WHERE Channel='" << i << "' AND Mass_ID='" << Mass_ID << "' AND PMT_ID ='" << PMT_ID << "'";
-		//cout << SS.str() << endl;
+		SS << "SELECT * FROM " << DATABASE_TABLE << " WHERE Channel='" << i << "' AND Mass_ID='" << massNumber << "' AND PMT_ID ='" << PMT_ID[i] << "'";
+		cout << SS.str() << endl;
 		string QuerySQL = SS.str();
 		Query query = conn.query(QuerySQL);
         StoreQueryResult res = query.store();
-		if ( res.num_rows() > 0)	//?¤æ??????????		{ 
+		if ( res.num_rows() > 0)
+			{ 
 			cout << "This record is already existed!" << endl;
 			SS.str("");
-			string UpdateSQL="UPDATE container_test SET ";
-			SS << UpdateSQL << "Mcp_Hama='" << Mcp_Hama << "',HV='" << HV << "',Gain_at_0_1pe='" << GAIN << "',Mu_at_0_1pe='" << Mu << "',CF_at_0_1pe='" << CF << "',Mu_Charge_at_0_1pe='" << Mu_charge << "',CF_charge_at_0_1pe='" << CF_charge << "',Mean_charge_at_0_1pe='" << Mean_charge << "',Amplitude_at_0_1pe='" << Amplitude << "',Resolution_at_0_1pe='" << Resolution << "',PvsV_at_0_1pe='" << P_V << "',SvsN_at_0_1pe='" << S_N<< "',Risetime_at_0_1pe='" << Risetime << "',Falltime_at_0_1pe='" << Falltime << "',FWHM_at_0_1pe='" << FWHM_NEW << "',Prepulse_proportion_at_0_1pe='"<< Prepulse_proportion << "',Dark_rate_at_0_1pe='" << Dark_rate << "',Pedestal_chi2_ndf_at_0_1pe='" << Pedestal_chi2_ndf << "',Signal_chi2_ndf_at_0_1pe='" << Signal_chi2_ndf << "',PDE_at_0_1pe='" << PDE << "',Hittime_at_0_1pe='" << Hittime << "',TTS_LED_at_0_1pe='" << TTS << "',Prepulse_proportion_sub_at_0_1pe='" << Prepulse_proportion_sub << "' WHERE Channel='" << i << "' AND Mass_ID='" << Mass_ID << "' AND PMT_ID ='" << PMT_ID << "'";
+			string UpdateSQL="UPDATE ";
+			SS << UpdateSQL << DATABASE_TABLE << " SET " << "Gain_at_setpoints3000='" << Gain[i] << "',Mu_at_setpoints3000='" << Mu[i] << "',CF_at_setpoints3000='" << CF[i] << "',Mu_Charge_at_setpoints3000='" << Mu_charge[i] << "',CF_charge_at_setpoints3000='" << CF_charge[i] << "',Mean_charge_at_setpoints3000='" << Mean_charge[i] << "',Amplitude_at_setpoints3000='" << Amplitude[i] << "',Resolution_at_setpoints3000='" << Resolution[i] << "',PvsV_at_setpoints3000='" << P_V[i] << "',SvsN_at_setpoints3000='" << S_N[i] << "',Risetime_at_setpoints3000='" << Risetime[i] << "',Falltime_at_setpoints3000='" << Falltime[i] << "',FWHM_at_setpoints3000='" << FWHM_NEW[i] << "',Prepulse_proportion_at_setpoints3000='" << Prepulse_proportion[i] << "',Dark_rate_at_setpoints3000='" << Dark_rate[i] << "',Pedestal_chi2_ndf_at_setpoints3000='" << Pedestal_chi2_ndf[i] << "',Signal_chi2_ndf_at_setpoints3000='" << Signal_chi2_ndf[i] << "',PDE_at_setpoints3000='" << PDE[i] << "',Hittime_at_setpoints3000='" << Hittime[i] << "',TTS_LED_at_setpoints3000='" << TTS[i] << "',Prepulse_proportion_sub_at_setpoints3000='" << Prepulse_proportion_sub[i] << "' WHERE Channel='" << i << "' AND Mass_ID='" << massNumber << "' AND PMT_ID ='" << PMT_ID[i] << "'";
 			UpdateSQL = SS.str();
-			//cout << UpdateSQL << endl;
+			cout << UpdateSQL << endl;
 			Query query1 = conn.query(UpdateSQL);
-			cout << "Update Success!" << endl;
-		}
+			StoreQueryResult res1 = query1.store();
+			cout << PMT_ID << " Update Success!" << endl;
+			}
 		else 
-		{ 
+			{ 
 			cout << "This record is not existed!" << endl;
 			SS.str("");
-			string InsertSQL="INSERT INTO container_test(Date,Channel,Mass_ID,PMT_ID,MCP_Hama,HV,Gain_at_0_1pe,Mu_at_0_1pe,CF_at_0_1pe,Mu_Charge_at_0_1pe,CF_charge_at_0_1pe,Mean_charge_at_0_1pe,Amplitude_at_0_1pe,Resolution_at_0_1pe,PvsV_at_0_1pe,SvsN_at_0_1pe,Risetime_at_0_1pe,Falltime_at_0_1pe,FWHM_at_0_1pe,Prepulse_proportion_at_0_1pe,Dark_rate_at_0_1pe,Pedestal_chi2_ndf_at_0_1pe,Signal_chi2_ndf_at_0_1pe,PDE_at_0_1pe,Hittime_at_0_1pe,TTS_LED_at_0_1pe,Prepulse_proportion_sub_at_0_1pe) VALUES ('";
-			SS << InsertSQL << Date << "','" << i << "','" << Mass_ID << "','" << PMT_ID << "','" << Mcp_Hama << "','" << HV << "','" << GAIN << "','" << Mu << "','" << CF << "','" << Mu_charge << "','" << CF_charge << "','" << Mean_charge << "','" << Amplitude << "','" << Resolution << "','" << P_V << "','" << S_N << "','" << Risetime << "','" << Falltime << "','" << FWHM_NEW << "','" << Prepulse_proportion << "','" << Dark_rate << "','" << Pedestal_chi2_ndf << "','" << Signal_chi2_ndf << "','" << PDE << "','" << Hittime << "','" << TTS << "','" << Prepulse_proportion_sub << "')";
+			string InsertSQL="INSERT INTO ";
+			SS << InsertSQL << DATABASE_TABLE << "(Channel,Mass_ID,PMT_ID,MCP_Hama,HV,Gain_at_setpoints3000,Mu_at_setpoints3000,CF_at_setpoints3000,Mu_Charge_at_setpoints3000,CF_charge_at_setpoints3000,Mean_charge_at_setpoints3000,Amplitude_at_setpoints3000,Resolution_at_setpoints3000,PvsV_at_setpoints3000,SvsN_at_setpoints3000,Risetime_at_setpoints3000,Falltime_at_setpoints3000,FWHM_at_setpoints3000,Prepulse_proportion_at_setpoints3000,Dark_rate_at_setpoints3000,Pedestal_chi2_ndf_at_setpoints3000,Signal_chi2_ndf_at_setpoints3000,PDE_at_setpoints3000,Hittime_at_setpoints3000,TTS_LED_at_setpoints3000,Prepulse_proportion_sub_at_setpoints3000) VALUES ('" <<  i << "','" << massNumber << "','" << PMT_ID[i] << "','" << Mcp_Hama << "','" << HV << "','" << Gain[i] << "','" << Mu[i] << "','" << CF[i] << "','" << Mu_charge[i] << "','" << CF_charge[i] << "','" << Mean_charge[i] << "','" << Amplitude[i] << "','" << Resolution[i] << "','" << P_V[i] << "','" << S_N[i] << "','" << Risetime[i] << "','" << Falltime[i] << "','" << FWHM_NEW[i] << "','" << Prepulse_proportion[i] << "','" << Dark_rate[i] << "','" << Pedestal_chi2_ndf[i] << "','" << Signal_chi2_ndf[i] << "','" << PDE[i] << "','" << Hittime[i] << "','" << TTS[i] << "','" << Prepulse_proportion_sub[i] << "')";
 			InsertSQL = SS.str();
-			//cout << InsertSQL << endl;
+			cout << InsertSQL << endl;
 			Query query2 = conn.query(InsertSQL);
-			cout << "Insert Success!" << endl;
+			StoreQueryResult res2 = query2.store();
+			cout << PMT_ID << " Insert Success!" << endl;
+			}
 		}
-	}
 	else
-	{
+		{
 		cout << "DB connection fault!" << endl;
-	};	
-        }
+		};	
+    }
     if (i==131)
        {outFile<<i<<"  "<<massNumber<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<"  "<<"NAN"<<endl;
     }
